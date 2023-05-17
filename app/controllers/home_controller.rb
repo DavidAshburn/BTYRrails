970f0b4de935a15b0f2db446710ece10a0036c8a
DavidAshburn <oneinchwebbing@gmail.com>
2023-05-17 01:20:12 -0700

    started on index page layout, images dont work

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def store
+  end
+  def education
+  end
+  def rigging
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/store'
+  get 'home/education'
+  get 'home/rigging'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
